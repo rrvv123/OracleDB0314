159d796faf5903ebf702d482ee58f6c7b9c8f1f5
rrvv123 <eorms7391@gmail.com>
2024-03-21 11:35:16 +0900

    0321 총강의 내용 주석처리

--- src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
+++ src/test/java/kr/ac/kopo/oracledb0314/repository/MemoRepositoryTests.java
@@ -21,6 +21,7 @@ public class MemoRepositoryTests {
         System.out.println(memoRepository.getClass().getName());
     }
 
+    // MemoRepository의 save/Memo Entity 객체의 참조값을 호출해서 insert한다.
     @Test
     public void testInsertDummies(){
         IntStream.rangeClosed(1, 100).forEach(i -> {
@@ -29,6 +30,9 @@ public class MemoRepositoryTests {
         });
     }
 
+    //MemoRepository의 findById(Memo Entity 객체의 Id로 설정된 필드값)를 호출해서 select한다.
+    //findById()호출되면 바로 select문을 실행한다.
+    //findById()는 NULLPointerException이 발생되지 않도록 Null 체크를 한다.
     @Test
     public void testSelect(){
         Long mno = 100L;
@@ -44,6 +48,8 @@ public class MemoRepositoryTests {
         }
     }
 
+    //MemoRepository의 getOne(Memo Entity 객체의 Id로 설정된 필드값)를 호출해서 select한다.
+    //getOne()은 호출되면 바로 실행되지 않고 Memo Entity가 필요할 때 select를 실행한다.
     @Test
     @Transactional
     public void testSelect2(){
@@ -56,6 +62,8 @@ public class MemoRepositoryTests {
         System.out.println(memo);
     }
 
+    // MemoRepository의 save/Memo Entity 객체의 참조값을 호출해서 update한다.
+    // save()는 호출되면 먼저 select를 하기 때문에 기존에 Entity가 있을 때는 update를 실행한다.
     @Test
     public void testUpdate(){
         Memo memo = Memo.builder().mno(95L).memoText("Update Dummy Data 95").build();
@@ -63,5 +71,13 @@ public class MemoRepositoryTests {
         Memo memo1 = memoRepository.save(memo);
 
         System.out.println(memo1);
+    }
+
+    // MemoRepository의 deleteById(Memo Entity의 mno 값을) 호출해서 delete한다.
+    @Test
+    public void testDelete(){
+        Long mno = 100L;
+
+        memoRepository.deleteById(mno);
     }
 }
